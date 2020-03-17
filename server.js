ed0ad6e53c66f098f03782fce48d9502d8b9a639
Alfredo Savi <alfredosavi@hotmail.com>
2020-03-17 02:04:08 -0300

    commit do data

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const { Router } = require("express");
+const cors = require("cors");
+const axios = require("axios");
 const server = express();
-server.use(express.json());
 
 // configurar o servidor para apresentar arquivos como JS, CSS
+server.use(express.json());
+server.use(cors());
 server.use(express.static("public"));
-
 // Habilitar Body do formulario
 server.use(express.urlencoded({ extended: true }));
 
-server.use("/", require("./routes"));
+server.get("/", async (req, res) => {
+  const teste = await axios.get(
+    "https://thevirustracker.com/free-api?global=stats"
+  );
+  console.log(teste);
+  return res.json(teste.data);
+});
 
 server.listen(3000);
