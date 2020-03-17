3c102f885efcd1c5c88083f22fa339d43e405f6d
Gabriel Prando <gprando55@gmail.com>
2020-03-17 14:50:40 -0300

    dados por pais e global

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,25 @@
 const axios = require('axios');
 
-class App {
-  constructor() {
-    this.infos = axios.get(`https://thevirustracker.com/free-api?global=stats`);
+async function getGlobalDados() {
+  try {
+    const response = await axios.get(`https://thevirustracker.com/free-api?global=stats`);
 
-    this.dadosTela = document.getElementById('')
-
-    this.registreDados();
-  }
-
-  registreDados() {
-    this.dadosTela.onload()
+    console.log(response.data.results[0]);
+  } catch (err) {
+    console.log("erro");
   }
 }
 
-new App();
+getGlobalDados();
 
+async function getDadoPais(pais) {
+  try {
+    const response = await axios.get(`https://thevirustracker.com/free-api?countryTotal=${pais}`);
 
+    console.log(response.data.countrydata[0]);
+  } catch (err) {
+    console.log("Esse país não existe");
+  }
+}
 
-
-
-
-// const axios = require('axios');
-
-// class Ap {
-//   static async getInfo() {
-//     const response = await axios.get(`https://thevirustracker.com/free-api?global=stats`);
-    
-//     let {
-//       total_cases, 
-//       total_recovered,
-//       total_unresolved,
-//       total_deaths,
-//       total_new_cases_today,
-//       total_new_deaths_today,
-//       total_active_cases,
-//       total_serious_cases
-//     } = response.data.results[0]
-
-
-//       console.log(total_active_cases)
-    
-
-//     // console.log(response.data.results[0]);
-//   }
-// }
-
-// Api.getInfo();
+getDadoPais("US");
