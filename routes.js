4ace0f591caec830828d137c8b8392bdea01f305
Alfredo Savi <alfredosavi@hotmail.com>
2020-03-17 01:33:13 -0300

    add

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
-
-const routes = new Router();
+const express = require("express");
+const axios = require("axios");
+const routes = express.Router();
 
 routes.get("/", async (req, res) => {
-  const teste = await axios.get("https://api.github.com/users/alfredosavi");
+  const teste = await axios.get("https://api.github.com/users/alfredosavi/");
   return res.json(teste);
 });
 
-export default routes;
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { Router } = require("express");
 const server = express();
+server.use(express.json());
 
 // configurar o servidor para apresentar arquivos como JS, CSS
 server.use(express.static("public"));
@@ -8,6 +9,6 @@ server.use(express.static("public"));
 // Habilitar Body do formulario
 server.use(express.urlencoded({ extended: true }));
 
-const routes = Router();
+server.use("/", require("./routes"));
 
 server.listen(3000);
