c6d9451e8d511f3a3c7ecafd39b3f46cd13b8d57
Gabriel Prando <gprando55@gmail.com>
2020-03-17 00:37:07 -0300

    mudança de cores

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const Pool = require("pg").Pool;
+const { Router } = require("express");
 const server = express();
-require("dotenv/config");
 
 // configurar o servidor para apresentar arquivos como JS, CSS
 server.use(express.static("public"));
@@ -9,45 +8,8 @@ server.use(express.static("public"));
 // Habilitar Body do formulario
 server.use(express.urlencoded({ extended: true }));
 
-const db = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE
-});
 
-const nunjucks = require("nunjucks");
-nunjucks.configure("./", {
-  express: server,
-  noCache: true
-});
+const routes = Router();
 
-server.get("/", (req, res) => {
-  db.query("SELECT * FROM donors", (err, result) => {
-    if (err) return res.send("err");
-
-    const donors = result.rows;
-    return res.render("index.html", { donors });
-  });
-});
-
-server.post("/", (req, res) => {
-  const { name, email, blood } = req.body;
-
-  if (name == "" || email == "" || blood == "") {
-    return res.send("Todos os campos são obrigatórios");
-  }
-
-  const query = `
-    INSERT INTO donors ("name", "email", "blood")
-    VALUES ($1, $2, $3)`;
-
-  db.query(query, [name, email, blood], err => {
-    if (err) return res.send(err);
-
-    return res.redirect("/");
-  });
-});
 
 server.listen(3000);
