a0cf9c4d138a40e5cc88afff4415776ed735d5cf
Alfredo Savi <alfredosavi@hotmail.com>
2020-03-17 21:50:52 -0300

    api

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,7 @@
 document.querySelector("header button").addEventListener("click", function() {
   document.querySelector(".form").classList.toggle("hide");
 });
+
+document.getElementById("pesquisar").addEventListener("click", function() {
+  document.getElementById("cabecalho").innerHTML = "treasd";
+});
--- server.js
+++ server.js
@@ -10,12 +10,29 @@ server.use(express.static("public"));
 // Habilitar Body do formulario
 server.use(express.urlencoded({ extended: true }));
 
-server.get("/", async (req, res) => {
-  const teste = await axios.get(
-    "https://thevirustracker.com/free-api?global=stats"
-  );
-  console.log(teste);
-  return res.json(teste.data);
+const nunjucks = require("nunjucks");
+nunjucks.configure("./", {
+  express: server,
+  noCache: true
 });
 
+server.get("/", async (req, res) => {
+  const api = await axios.get(
+    "https://thevirustracker.com/free-api?global=stats"
+  );
+
+  const dados = api.data.results[0]; // não esquecer ... ele retorna um vetor ... os dados estão no indice 0
+  // console.log(dados);
+  return res.render("index.html", { dados }); // passando dados api para o index via nunjunks
+});
+
+server.post("/", async (req, res) => {
+  const { paises } = req.body;
+  const api = await axios.get(
+    `https://thevirustracker.com/free-api?countryTotal=${paises}`
+  );
+  // console.log(api.data.countrydata[0]);
+  const dados = api.data.countrydata[0];
+  return res.render("index.html", { dados });
+});
 server.listen(3000);
